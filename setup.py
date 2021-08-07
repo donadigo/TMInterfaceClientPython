ca73562675f720d07af5be00489af556d007347f
Adam Bieńkowski <donadigos159@gmail.com>
2021-08-08 00:06:48 +0200

    Make a Python package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name = 'tminterface',
+    packages = ['tminterface'],
+    version = '0.1',
+    license='GPL3',
+    description = 'A client for TMInterface, a TrackMania TAS tool',
+    author = 'Adam Bieńkowski',
+    author_email = 'donadigos159@gmail.com',
+    url = 'https://github.com/donadigo/TMInterfaceClient',
+    keywords = ['TMInterface', 'client', 'TrackMania'],
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
