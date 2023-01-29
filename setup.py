3e25c25b32397ad0111af8df4449497aae48aed1
Adam Bieńkowski <donadigos159@gmail.com>
2023-01-29 21:31:33 +0100

    Release 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'tminterface',
     packages = ['tminterface'],
-    version = '1.0.1',
+    version = '1.0.2',
     license='GPL3',
     description = 'A client for TMInterface, a TrackMania TAS tool',
     author = 'Adam Bieńkowski',
